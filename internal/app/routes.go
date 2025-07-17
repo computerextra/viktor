eb106e060c2c73538d88c769419e1b6abb6919de
jayzone91 <register@j4yz0n3.de>
2025-07-17 17:19:29 +0200

    added auth ti sensitive sites

--- internal/app/routes.go
+++ internal/app/routes.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/computerextra/viktor/internal/handler"
+	"github.com/computerextra/viktor/internal/middleware"
 )
 
 func (a *App) loadRoutes() (http.Handler, error) {
@@ -61,42 +62,42 @@ func (a *App) loadPages(router *http.ServeMux) {
 	// CMS
 	router.HandleFunc("GET /CMS", h.GetCmsCounts) // CMS Übersicht
 	// Abteilungen
-	router.HandleFunc("GET /CMS/Abteilungen", h.GetAbteilungen)               // Abteilungsübersicht
-	router.HandleFunc("GET /CMS/Abteilungen/Neu", h.NewAbteilung)             // Neue Abteilung Formular
-	router.HandleFunc("POST /CMS/Abteilungen/Neu", h.CreateAbteilung)         // Abteilung anlegen
-	router.HandleFunc("GET /CMS/Abteilungen/{id}", h.GetAbteilung)            // Abteilung bearbeiten Form
-	router.HandleFunc("POST /CMS/Abteilungen/{id}", h.UpdateAbteilung)        // Abteilung bearbeiten
-	router.HandleFunc("POST /CMS/Abteilungen/{id}/Delete", h.DeleteAbteilung) // Abteilung löschen
+	router.HandleFunc("GET /CMS/Abteilungen", h.GetAbteilungen)                     // Abteilungsübersicht
+	router.HandleFunc("GET /CMS/Abteilungen/Neu", middleware.Auth(h.NewAbteilung))  // Neue Abteilung Formular
+	router.HandleFunc("POST /CMS/Abteilungen/Neu", h.CreateAbteilung)               // Abteilung anlegen
+	router.HandleFunc("GET /CMS/Abteilungen/{id}", middleware.Auth(h.GetAbteilung)) // Abteilung bearbeiten Form
+	router.HandleFunc("POST /CMS/Abteilungen/{id}", h.UpdateAbteilung)              // Abteilung bearbeiten
+	router.HandleFunc("POST /CMS/Abteilungen/{id}/Delete", h.DeleteAbteilung)       // Abteilung löschen
 	// Angebote
-	router.HandleFunc("GET /CMS/Angebote", h.GetAngebote)                // Angebotsübersicht
-	router.HandleFunc("GET /CMS/Angebote/Neu", h.NewAngebot)             // Neue Angebot Formular
-	router.HandleFunc("POST /CMS/Angebote/Neu", h.CreateAngebot)         // Angebot anlegen
-	router.HandleFunc("GET /CMS/Angebote/{id}", h.GetAngebot)            // Angebot bearbeiten Form
-	router.HandleFunc("POST /CMS/Angebote/{id}", h.UpdateAngebot)        // Angebot bearbeiten
-	router.HandleFunc("POST /CMS/Angebote/{id}/Toggle", h.ToggleAngebot) // Angebot bearbeiten
-	router.HandleFunc("POST /CMS/Angebote/{id}/Delete", h.DeleteAngebot) // Angebot bearbeiten
+	router.HandleFunc("GET /CMS/Angebote", h.GetAngebote)                      // Angebotsübersicht
+	router.HandleFunc("GET /CMS/Angebote/Neu", middleware.Auth(h.NewAngebot))  // Neue Angebot Formular
+	router.HandleFunc("POST /CMS/Angebote/Neu", h.CreateAngebot)               // Angebot anlegen
+	router.HandleFunc("GET /CMS/Angebote/{id}", middleware.Auth(h.GetAngebot)) // Angebot bearbeiten Form
+	router.HandleFunc("POST /CMS/Angebote/{id}", h.UpdateAngebot)              // Angebot bearbeiten
+	router.HandleFunc("POST /CMS/Angebote/{id}/Toggle", h.ToggleAngebot)       // Angebot bearbeiten
+	router.HandleFunc("POST /CMS/Angebote/{id}/Delete", h.DeleteAngebot)       // Angebot bearbeiten
 	// Jobs
-	router.HandleFunc("GET /CMS/Jobs", h.GetJobs)                // Jobübersicht
-	router.HandleFunc("GET /CMS/Jobs/Neu", h.NewJob)             // Neuer Job Formular
-	router.HandleFunc("POST /CMS/Jobs/Neu", h.CreateJob)         // Job anlegen
-	router.HandleFunc("GET /CMS/Jobs/{id}", h.GetJob)            // Job bearbeiten Form
-	router.HandleFunc("POST /CMS/Jobs/{id}", h.UpdateJob)        // Job bearbeiten
-	router.HandleFunc("POST /CMS/Jobs/{id}/Toggle", h.ToggleJob) // Job Toggle
-	router.HandleFunc("POST /CMS/Jobs/{id}/Delete", h.DeleteJob) // Job löschen
+	router.HandleFunc("GET /CMS/Jobs", h.GetJobs)                      // Jobübersicht
+	router.HandleFunc("GET /CMS/Jobs/Neu", middleware.Auth(h.NewJob))  // Neuer Job Formular
+	router.HandleFunc("POST /CMS/Jobs/Neu", h.CreateJob)               // Job anlegen
+	router.HandleFunc("GET /CMS/Jobs/{id}", middleware.Auth(h.GetJob)) // Job bearbeiten Form
+	router.HandleFunc("POST /CMS/Jobs/{id}", h.UpdateJob)              // Job bearbeiten
+	router.HandleFunc("POST /CMS/Jobs/{id}/Toggle", h.ToggleJob)       // Job Toggle
+	router.HandleFunc("POST /CMS/Jobs/{id}/Delete", h.DeleteJob)       // Job löschen
 	// Mitarbeiter
-	router.HandleFunc("GET /CMS/Mitarbeiter", h.GetMitarbeitersWithAbteilung)   // Mitarbeiterübersicht
-	router.HandleFunc("GET /CMS/Mitarbeiter/Neu", h.NewMitarbeiter)             // Neuer Mitarbeiter Formular
-	router.HandleFunc("POST /CMS/Mitarbeiter/Neu", h.CreateMitarbeiter)         // Mitarbeiter anlegen
-	router.HandleFunc("GET /CMS/Mitarbeiter/{id}", h.GetMitarbeiter)            // Mitarbeiter bearbeiten Form
-	router.HandleFunc("POST /CMS/Mitarbeiter/{id}", h.UpdateMitarbeiter)        // Mitarbeiter bearbeiten
-	router.HandleFunc("POST /CMS/Mitarbeiter/{id}/Delete", h.DeleteMitarbeiter) // Mitarbeiter löschen
+	router.HandleFunc("GET /CMS/Mitarbeiter", h.GetMitarbeitersWithAbteilung)         // Mitarbeiterübersicht
+	router.HandleFunc("GET /CMS/Mitarbeiter/Neu", middleware.Auth(h.NewMitarbeiter))  // Neuer Mitarbeiter Formular
+	router.HandleFunc("POST /CMS/Mitarbeiter/Neu", h.CreateMitarbeiter)               // Mitarbeiter anlegen
+	router.HandleFunc("GET /CMS/Mitarbeiter/{id}", middleware.Auth(h.GetMitarbeiter)) // Mitarbeiter bearbeiten Form
+	router.HandleFunc("POST /CMS/Mitarbeiter/{id}", h.UpdateMitarbeiter)              // Mitarbeiter bearbeiten
+	router.HandleFunc("POST /CMS/Mitarbeiter/{id}/Delete", h.DeleteMitarbeiter)       // Mitarbeiter löschen
 	// Partner
-	router.HandleFunc("GET /CMS/Partner", h.GetPartners)                // Partnerübersicht
-	router.HandleFunc("GET /CMS/Partner/Neu", h.NewPartner)             // Neuer Partner Formular
-	router.HandleFunc("POST /CMS/Partner/Neu", h.CreatePartner)         // Partner anlegen
-	router.HandleFunc("GET /CMS/Partner/{id}", h.GetPartner)            // Partner bearbeiten Form
-	router.HandleFunc("POST /CMS/Partner/{id}", h.UpdatePartner)        // Partner bearbeiten
-	router.HandleFunc("POST /CMS/Partner/{id}/Delete", h.DeletePartner) // Partner löschen
+	router.HandleFunc("GET /CMS/Partner", h.GetPartners)                      // Partnerübersicht
+	router.HandleFunc("GET /CMS/Partner/Neu", middleware.Auth(h.NewPartner))  // Neuer Partner Formular
+	router.HandleFunc("POST /CMS/Partner/Neu", h.CreatePartner)               // Partner anlegen
+	router.HandleFunc("GET /CMS/Partner/{id}", middleware.Auth(h.GetPartner)) // Partner bearbeiten Form
+	router.HandleFunc("POST /CMS/Partner/{id}", h.UpdatePartner)              // Partner bearbeiten
+	router.HandleFunc("POST /CMS/Partner/{id}/Delete", h.DeletePartner)       // Partner löschen
 
 	// Archive
 	router.HandleFunc("GET /Archiv", h.Archive)
@@ -104,7 +105,7 @@ func (a *App) loadPages(router *http.ServeMux) {
 	router.HandleFunc("GET /Archiv/{id}", h.GetArchive)
 
 	// Warenlieferung
-	router.HandleFunc("GET /Warenlieferung", h.Warenlieferung)
+	router.HandleFunc("GET /Warenlieferung", middleware.Auth(h.Warenlieferung))
 	router.HandleFunc("POST /Warenlieferung/Generate", h.GenerateWarenlieferung)
 	router.HandleFunc("POST /Warenlieferung/Send", h.SendWarenlieferung)
 
@@ -126,19 +127,19 @@ func (a *App) loadPages(router *http.ServeMux) {
 	router.HandleFunc("POST /Lieferanten/{id}/{aid}/Delete", h.DeleteAnsprechpartner)
 
 	// Seriennummern
-	router.HandleFunc("GET /Seriennummer", h.Seriennummern)
+	router.HandleFunc("GET /Seriennummer", middleware.Auth(h.Seriennummern))
 	router.HandleFunc("POST /Seriennummer", h.SearchSeriennummer)
 
 	// Info an Kunde
-	router.HandleFunc("GET /Info", h.Info)
+	router.HandleFunc("GET /Info", middleware.Auth(h.Info))
 	router.HandleFunc("POST /Info", h.SendInfo)
 
 	// Label Sync
-	router.HandleFunc("GET /Label", h.Label)
+	router.HandleFunc("GET /Label", middleware.Auth(h.Label))
 	router.HandleFunc("POST /Label", h.SyncLabel)
 
 	// Aussteller
-	router.HandleFunc("GET /Aussteller", h.Aussteller)
+	router.HandleFunc("GET /Aussteller", middleware.Auth(h.Aussteller))
 	router.HandleFunc("POST /Aussteller/Sync", h.SyncAussteller)
 	router.HandleFunc("POST /Aussteller/Update", h.UpdateAussteller)
 	router.HandleFunc("POST /Aussteller/Upload", h.UploadAussteller)
